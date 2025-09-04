555fabeb86efd77ca41d4e1f9cfcef9b520a32f8
Lucasheg <lucasheg@icloud.com>
2025-09-04 12:43:44 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+<form
+  name="contact"
+  data-netlify="true"
+  netlify-honeypot="bot-field"
+  onSubmit={submit}
+  className="card p-6 grid grid-cols-1 md:grid-cols-2 gap-4"
+>
+  <input type="hidden" name="form-name" value="contact" />
+  <input type="hidden" name="bot-field" />
+  {/* ...rest of the fields stay the same... */}
